da81d0bcaa78863e32b58dc45bcfef325412a126
João Vítor <joaovitordeaquino18@gmail.com>
2025-02-21 16:19:50 -0300

    finalizado

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -5,9 +5,9 @@ import axios from "axios"
 // const {NODE_ENV} = process.env
 // const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api" DEU ERRADO
 
-// const URL = "https://spotifyfullstack-55jz.onrender.com/api"
+const URL = "https://spotifyfullstack-55jz.onrender.com/api"
 
-const URL = "http://localhost:3000/api"
+// const URL = "http://localhost:3000/api"
 
 const responseArtists = await axios.get(`${URL}/artists`)
 const responseSongs = await axios.get(`${URL}/songs`)
