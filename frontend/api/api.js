9fd03850010c5ef35b89c7098e8bfde0022e3cb1
João Vítor <joaovitordeaquino18@gmail.com>
2025-02-21 15:13:45 -0300

    ajuste no process

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -2,8 +2,10 @@
 import "dotenv/config"
 import axios from "axios"
 
-const {NODE_ENV} = process.env
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api"
+// const {NODE_ENV} = process.env
+// const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api" DEU ERRADO
+
+const URL = "https://spotifyfullstack-55jz.onrender.com/api"
 
 const responseArtists = await axios.get(`${URL}/artists`)
 const responseSongs = await axios.get(`${URL}/songs`)
