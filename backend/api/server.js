31f0d101a038b03d1eb3c8d985a384c2996ff1fe
João Vítor <joaovitordeaquino18@gmail.com>
2025-02-21 14:54:04 -0300

    update

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,19 +1,28 @@
 import express from "express"
 import cors from "cors"
 import { db } from "./connect.js";
+import path from "path";
 const app = express();
 const port = 3000
+const __dirname = path.resolve()
 
 app.use(cors())
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
     response.send("Só vamos usar os endpoints: '/artists' e '/songs'!")
 })
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
     response.send(await db.collection("artists").find({}).toArray())
 })
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
     response.send(await db.collection("songs").find({}).toArray())
+})
+
+// deploy - quando acessar qualquer endpoint que nao seja os acima, executa isso aqui:
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+app.get("*", async (request, response) => {
+    response.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
 })
 
 app.listen(port, () =>{
